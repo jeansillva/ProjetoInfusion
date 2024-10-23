c05263b3c3e4f82084a8ac2c5610c458502bc5da
Jean <jeancarlos_bh@outlook.com>
2024-10-23 19:27:24 -0300

    Primeira implementação JS
    Habilitando classes pre-preparadas para comportamento do site corresponder a presença do JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+    var $body = document.querySelector('body')
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+
+    var $btnMenu = document.querySelector('.header__btnMenu')
+    $btnMenu.removeAttribute('style')
+
+    var menu = new Menu({
+        container: '',
+        toggleBtn: '',
+        widthEnabled: '',
+    })
+})()
